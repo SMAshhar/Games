71ce86f02814b8dd54aa65ca95762de46c8f1071
SMAshhar <63975782+SMAshhar@users.noreply.github.com>
2020-07-29 13:37:47 +0500

    Final Update for Warrior.py

--- Warrior/Warrior.py
+++ Warrior/Warrior.py
@@ -1,6 +1,6 @@
 import random
 import csv
-print("Hellow WOrld!")
+# print("Hellow WOrld!")
 # dutring the pendamic.
 
 Specs = []
@@ -15,11 +15,11 @@ print(Specs)
 yu = input("\tGo through the following information carefully : ")
 
 guide = input("""
-    In each instent, you will face a randomly generated enemy.
+    In each instence, you will face a randomly generated enemy.
     Each enemy has its own points, life and damage.
     You have a few abilities that you should use to kill the enemy and protect yourself.
-    Whoever kills the other first wins. 
-    Be advised to correctly spell your spells or the enemy will take advantage of it.
+    Kill your foe to continue your adventure. 
+    Be advised to correctly spell your abilities or the enemy will take advantage of it.
     
     Good luck
     """)
@@ -29,7 +29,7 @@ for a in Specs:
     print("Power", x, "=", a)           # Instructions on details of abilities
     x += 1
 
-input("\tPress return key to enter your World : ")
+input("\nPress return key to enter your World : \n")
 
 
 class Enemy():                              # Enemy generation tamplate
@@ -42,56 +42,90 @@ class Enemy():                              # Enemy generation tamplate
     def attack(self):
         return random.randint(self.damage - 10, self.damage+50)
 
-
-Enemy1 = Enemy(40, random.randint(300, 400), 5, "Slime")
-Enemy2 = Enemy(70, random.randint(400, 550), 10, "Goat")
-Enemy3 = Enemy(80, random.randint(550, 700), 15, "Lion")
-Enemy4 = Enemy(100, random.randint(900, 1200), 25, "Minotor")
-
-x = random.randint(1, 4)
-
-if x == 1:
-    y = Enemy1
-elif x == 2:
-    y = Enemy2
-elif x == 3:
-    y = Enemy3
-elif x == 4:
-    y = Enemy4
-
-
 class Charactor():
-    def __init__(self, HP, Damage):
-        self.HP = HP
-        self.Damage = Damage
+        def __init__(self, HP, Damage):
+            self.HP = HP
+            self.Damage = Damage
 
-
+points = 0
+asd = ""
 Warrior = Charactor(random.randint(500, 700), 70)
 
-print(f"A {y.species} jumps out of the bushes.")
+while asd.lower() != "q": 
 
-print("Your HP is :", Warrior.HP)
-print(f"{y.species} HP is :", y.HP)
+    x = random.randint(1, 4)
 
-print("You charged at the enemy, enemy misses its first turn.")
-x = input()
-y.HP -= random.randint(Warrior.Damage , Warrior.Damage + 70 )
+    Enemy1 = Enemy(40, random.randint(300, 400), 5, "Slime")
+    Enemy2 = Enemy(70, random.randint(400, 550), 10, "Goat")
+    Enemy3 = Enemy(80, random.randint(550, 700), 15, "Lion")
+    Enemy4 = Enemy(100, random.randint(900, 1200), 25, "Minotor")
 
-print("Warrior's HP = ", int(Warrior.HP))
-print(f"{y.species}'s HP = ", int(y.HP))
-
-ability = input("What are you gonna do? : ")
-
-if ability == "rage":
-    print("You used Rage ability, increases your damage and attack twice")
-    y.HP -= 1.2 * Warrior.Damage
-    print()
-    y.HP -= 1.2 * Warrior.Damage
-    Warrior.HP -= random.randint(y.damage-15, y.damage+15)
-    print("Warrior's HP = ", int(Warrior.HP))
-    print(f"{y.species}'s HP = ", int(y.HP))
-elif ability == "bash":
-    print("Enemy is stunned for 2 turns")
-    y.HP -= 0.5 * Warrior.Damage
+    if x == 1:
+        y = Enemy1
+    elif x == 2:
+        y = Enemy2
+    elif x == 3:
+        y = Enemy3
+    elif x == 4:
+        y = Enemy4
 
     
+
+    print(f"A {y.species} jumps out of the bushes.\n")
+
+    print("Your HP is :", Warrior.HP)
+    print(f"{y.species} HP is :", y.HP)
+
+    print("\nYou charged at the enemy, enemy misses its first turn.")
+    x = input()
+    y.HP -= random.randint(Warrior.Damage , Warrior.Damage + 70 )
+
+    print("Warrior's HP = ", Warrior.HP)
+    print(f"{y.species}'s HP = ", y.HP)
+    
+
+    print("\nWhat are you gonna do? : ")
+
+    while Warrior.HP > 0 and y.HP > 0:
+
+        ability = input("Your move for the next turn : ")
+
+        if ability == "rage":
+            print("You used Rage ability, increases your damage and you attack twice")
+            y.HP -= 1.2 * Warrior.Damage
+            points += y.point
+            print()
+            y.HP -= 1.2 * Warrior.Damage
+            points += y.point
+            Warrior.HP -= random.randint(y.damage-15, y.damage+15)
+            print("Warrior's HP = ", Warrior.HP)
+            print(f"{y.species}'s HP = ", y.HP)
+            print(points)
+
+        elif ability == "bash":
+            print("Enemy is stunned for this turns")
+            y.HP -= 0.5 * Warrior.Damage
+            points += y.point
+            print("Warrior's HP = ", Warrior.HP)
+            print(f"{y.species}'s HP = ", y.HP)
+            print(points)
+
+        else:
+            print("You got confused. Enemy wasted no chance to make use of it.")
+            Warrior.HP -= random.randint(y.damage-15, y.damage+15)
+            print("Warrior's HP = ", int(Warrior.HP))
+            print(f"{y.species}'s HP = ", int(y.HP))
+            print(points)
+
+        
+    if y.HP <= 0:
+        print(f"Victory, You have defeated {y.species}, your current points are {points}")
+    elif Warrior.HP <= 0:
+        print("You have died")
+        break
+    
+    asd = input("Press entre to continue, or 'Q' to quite : ")
+
+print(f"Total score is : ", points)
+print("GAME OVER")
+
