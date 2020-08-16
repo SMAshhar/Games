43878917e2b1a523fa258a5e462cffad0ad373a7
SMAshhar <63975782+SMAshhar@users.noreply.github.com>
2020-08-16 10:21:36 +0800

    Add files via upload

--- An_RPG.py
+++ An_RPG.py
@@ -1,1 +1,36 @@
 # This program is going to merge the three programs and generate a single game
+
+# Step 1: The registration process
+
+# # # # # Step 1 - 1: Defining a function/class, each for login and registeration.
+
+def login():
+    name = input("Username : ")
+    password = input("Password : ")
+    try:
+        with open(name".txt", "a") as f:
+    except FileNotFoundError:
+        print("User not registered yet")
+    except:
+        print("An error has occured. Please try again")
+
+
+
+
+print("""Enter 'r' to register a new user
+        Enter 'l' to login an existing user""")
+choice = input()
+if choice == "l":
+    print("Login: ")
+    name = input("Username : ")
+    password = input("Password : ")
+    try:
+        with open(name".txt", "a") as f:
+    except FileNotFoundError:
+        print("User not registered yet")
+    except:
+        print("An error has occured. Please try again")
+    
+
+
+regName = input()
